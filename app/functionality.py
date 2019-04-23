c20ced203841c9993afb14611705f42b2f35e156
Rylan Sturm <sturm.rw@gmail.com>
2019-04-22 23:04:37 -0600

    capability to go 15 minutes past shift (new 5S standard)

--- app/functionality.py
+++ app/functionality.py
@@ -160,7 +160,7 @@ class Timer:
     @staticmethod
     def get_tCycle():
         if not Timer.catch_up_mode:
-            Timer.tCycle = PCT.sequence_time() - int((Plan.now() - Timer.mark).total_seconds())
+            Timer.tCycle = (PCT.catch_up_pct * Partsper.partsper) - int((Plan.now() - Timer.mark).total_seconds())
         else:
             Timer.tCycle = PCT.sequence_time() - int((Plan.now() - Timer.mark).total_seconds())
         return Timer.tCycle
